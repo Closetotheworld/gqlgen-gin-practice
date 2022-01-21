66eb5d7888496c63b4efbc09197370e4cbf3cd0e
closetotheworld <wall.heo@lunit.io>
2022-01-21 17:42:00 +0900

    add dir structure for common

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"github.com/closetotheworld/gqlgen-gin-practice/api/v1/graph"
+	"github.com/closetotheworld/gqlgen-gin-practice/api/v1/graph/generated"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/closetotheworld/gqlgen-gin-practice/api/v1/graph"
-	"github.com/closetotheworld/gqlgen-gin-practice/api/v1/graph/generated"
 )
 
 const defaultPort = "8080"
+
+const (
+	V1QueryPath      = "/v1/query"
+	V1PlaygroundPath = "/v1"
+)
 
 func main() {
 	port := os.Getenv("PORT")
